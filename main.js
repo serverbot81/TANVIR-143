44852be9170076ae5030badcb2cf48d9696d38f3
serverbot81 <serverbot81@gmail.com>
2024-12-18 18:08:11 +0600

    Update main.js

--- main.js
+++ main.js
@@ -61,23 +61,23 @@ global.client = new Object({
   getTime: function(option) {
     switch (option) {
       case "seconds":
-        return `${moment.tz("Asia/Manila").format("ss")}`;
+        return `${moment.tz("Asia/Dhaka").format("ss")}`;
       case "minutes":
-        return `${moment.tz("Asia/Manila").format("mm")}`;
+        return `${moment.tz("Asia/Dhaka").format("mm")}`;
       case "hours":
-        return `${moment.tz("Asia/Manila").format("HH")}`;
+        return `${moment.tz("Asia/Dhaka").format("HH")}`;
       case "date":
-        return `${moment.tz("Asia/Manila").format("DD")}`;
+        return `${moment.tz("Asia/Dhaka").format("DD")}`;
       case "month":
-        return `${moment.tz("Asia/Manila").format("MM")}`;
+        return `${moment.tz("Asia/Dhaka").format("MM")}`;
       case "year":
-        return `${moment.tz("Asia/Manila").format("YYYY")}`;
+        return `${moment.tz("Asia/Dhaka").format("YYYY")}`;
       case "fullHour":
-        return `${moment.tz("Asia/Manila").format("HH:mm:ss")}`;
+        return `${moment.tz("Asia/Dhaka").format("HH:mm:ss")}`;
       case "fullYear":
-        return `${moment.tz("Asia/Manila").format("DD/MM/YYYY")}`;
+        return `${moment.tz("Asia/Dhaka").format("DD/MM/YYYY")}`;
       case "fullTime":
-        return `${moment.tz("Asia/Manila").format("HH:mm:ss DD/MM/YYYY")}`;
+        return `${moment.tz("Asia/Dhaka").format("HH:mm:ss DD/MM/YYYY")}`;
     }
   },
   timeStart: Date.now()
